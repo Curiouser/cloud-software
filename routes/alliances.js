0b797a706512e522f09134ff5517cefc442b3ced
Curiouser <soniacorne@protonmail.com>
2017-02-09 17:51:14 +0100

    Add alliances.js

--- routes/alliances.js
+++ routes/alliances.js
@@ -2,3 +2,10 @@ const AllianceDAO = require('../models/AllianceDAO');
 
 var express = require('express');
 var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	AllianceDAO.getAll()
+	.then((users) => {
+		res.send(users);
+	});
+});
