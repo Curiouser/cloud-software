caa51a9db7fb4749be59bebb546f8a03d5dba8c4
Curiouser <soniacorne@protonmail.com>
2017-02-09 17:52:18 +0100

    Add AllianceDAO.js

--- models/AllianceDAO.js
+++ models/AllianceDAO.js
@@ -2,4 +2,13 @@ const DB = require('../models/database'); // constante DB car on veut surtout pa
 
 module.exports = { //exporte toutes les méthodes définies ci dessous
 
+	getAll() {
+	return DB.query('SELECT * FROM alliances') //pas besoin de then, return db query est suffisant, on l'a fait juste pour l'exemple
+		.then((result) => { //une fois qu'on a le result de la query, on fait le .then
+			return result;
+		})
+		.catch((error) => {
+		throw error;
+		})
+	}
 }
